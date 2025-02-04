d80ee3b643b66c06be5e4223ea8012a5fde0c525
Beata <manska.b@gmail.com>
2025-02-04 17:40:19 +0100

    usermanagement

---
+++ frontend/src/components/UserManagement.vue
@@ -0,0 +1,58 @@
+<template>
+    <div>
+        <ul v-for="user in users" :key="user.id">
+            <li> {{ user.name }} - {{ user.id }}</li>
+            <button @click="deleteUser(user.id)">Usuń</button>
+            <button @click="editUser(user.id)">Edytuj</button>
+        </ul>
+
+        <input v-model="newUser.name" placeholder="Imię">
+        <input v-model="newUser.email" placeholder="Email">
+        <button @click="addUser(user.id)">Dodaj użytkownika</button>
+    </div>
+</template>
+
+<script>
+import { axios } from "axios";
+export default {
+    data() {
+        return {
+            users: [],
+            newUser: { name: '' }, { email: '' }
+    };
+},
+mounted() {
+    this.fetchUsers();
+
+},
+methods: {
+    fetchUsers(){
+        axios.get('http://13.93.205.222:8080/api/users')
+            .then(response => {
+                this.users = response.data;
+            });
+    },
+    addUser() {
+        axios.post('http://13.93.205.222:8080/api/users', this.newUser)
+            .then(() => {
+                this.users.push(this.newUser);
+                this.newUser = { name: '', email: '' };
+                this.fetchUsers();
+            });
+    }
+
+    deleteUser(id){
+        axios.delete('http://13.93.205.222:8080/api/users/${id}')
+        then(() => {
+            this.users = this.users.filter(user => user.id !== id)
+        });
+    }
+
+    editUser(user) {
+        this.newUser = { ...user }
+    }
+
+
+}
+};
+</script>
