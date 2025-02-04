86796512439c53737e8a642c9d6a3a642cc9f45b
Beata <manska.b@gmail.com>
2025-02-04 18:53:08 +0100

    bootstrap

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router';
-
+import { BootstrapVue3, IconsPlugin } from 'bootstrap-vue-3'
+import 'bootstrap/dist/css/bootstrap.css'
 
 const app = createApp(App);
+
+app.use(BootstrapVue3)
+app.use(IconsPlugin)
+
 app.use(router);
 app.mount('#app');
-
